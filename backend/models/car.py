fd7394b71319052b91d0856076bcfc580ec9cc16
wald <1000druid@gmail.com>
2022-02-17 18:54:06 +0100

    Post Car endpoint

--- backend/models/car.py
+++ backend/models/car.py
@@ -1,3 +1,5 @@
+from sqlalchemy.exc import IntegrityError
+
 from . import db
 
 
@@ -7,3 +9,23 @@ class Car(db.Model):
     model = db.Column(db.String, nullable=False)
     production_date = db.Column(db.DateTime, nullable=True)
     licence_number = db.Column(db.String(10), nullable=False)
+
+    @classmethod
+    def create_car(cls, new_car):
+        created = False
+        car_db = Car(
+            model=new_car["model"],
+            production_date=new_car["production_date"],
+            licence_number=new_car["licence_number"],
+        )
+        db.session.begin_nested()
+        try:
+            db.session.add(car_db)
+            db.session.commit()
+            created = True
+            return created
+        except IntegrityError:
+            db.session.rollback()
+            db.session.refresh(car_db)
+            created = False
+            return created
--- backend/schema/car_schema.py
+++ backend/schema/car_schema.py
@@ -1,10 +1,15 @@
 from marshmallow import Schema, fields
 
 
-
 class CarSchema(Schema):
     id = fields.Str(required=True)
     model = fields.Str(required=True)
     production_date = fields.DateTime(format="%Y-%m-%d", required=False)
     licence_number = fields.Str(required=True)
 
+
+class CarCreateRequest(Schema):
+    model = fields.Str(required=True)
+    production_date = fields.DateTime(format="%Y-%m-%d", required=False,
+                                      missing=None)
+    licence_number = fields.Str(required=True)
